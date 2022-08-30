6289c8646b6ebe17b252cdbafca25d35e1775dbe
ClancyCC <zqc867342167@gmail.com>
2022-08-30 12:57:06 +0800

    Site updated: 2022-08-30 12:56:52

--- js/sw_delete.js
+++ js/sw_delete.js
@@ -1,7 +1,9 @@
-navigator.serviceWorker
-  .getRegistrations()
-  .then((r) => {
-    for (let i of r) i.unregister()
-    console.log('注销成功')
-  })
-  .catch(() => console.log('注销失败'))
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker
+    .getRegistrations()
+    .then((r) => {
+      for (let i of r) i.unregister()
+      console.log('注销成功')
+    })
+    .catch(() => console.log('注销失败'))
+}
